6d9d7e1de90423c6a8badf06b0fe4bf1f3ab1485
LEE HO <dlho1222@naver.com>
2024-01-25 20:51:00 +0900

    [UI] : 시간포멧 변경

--- app/src/main/java/com/example/imagesearch/adapter/ImageAdapter.kt
+++ app/src/main/java/com/example/imagesearch/adapter/ImageAdapter.kt
@@ -7,15 +7,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.imagesearch.data.Document
 import com.example.imagesearch.databinding.ItemImageBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class ImageAdapter(private val items: MutableList<Document>, private val context: Context) :
     RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
     inner class ImageViewHolder(private val binding: ItemImageBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(document: Document) {
+            val dateTime = document.datetime
+            val inputDateTime = LocalDateTime.parse(dateTime, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+            val outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            val outputDateString = inputDateTime.format(outputFormatter)
             with(binding) {
                 tvSource.text = document.display_sitename
-                tvDate.text = document.datetime
+                tvDate.text = outputDateString.toString()
                 Glide.with(context)
                     .load(document.thumbnail_url)
                     .into(ivThumb)
